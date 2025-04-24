b684fba7329ebf7cf78679eec7cd8a8a57aaabc5
Syxfer <skittleghost123@gmail.com>
2025-04-24 17:45:10 -0500

    Update Infinity-Cookies.js

--- Infinity-Cookies.js
+++ Infinity-Cookies.js
@@ -2,7 +2,7 @@ function main() {
   try {
     Game.cookies = Infinity
     console.log("Made By Syxfer");
-    console.alert("Made By Syxfer")
+    alert("Made By Syxfer")
     // Be careful! Calling main() inside main() will cause an infinite loop
     // and eventually crash your program due to stack overflow.
     // If you intended to call it only once, remove this line:
